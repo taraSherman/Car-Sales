45074e76b01526c38cbb54d9946541194e2de43f
Tara Sherman <taradelin@gmail.com>
2019-10-24 17:54:49 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { addFeature, removeFeature } from './actions'
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
@@ -7,7 +8,7 @@ import Total from './components/Total';
 
 const App = props => {
   const removeFeature = item => {
-    props.removeFeature(item)
+    props.removeFeature(item)    
   };
 
   const buyItem = item => {
@@ -18,25 +19,22 @@ const App = props => {
     <div className="boxes">
       <div className="box">
         <Header car={props.car} />
-        <AddedFeatures car={props.car} removeFeature={ removeFeature } />
+        <AddedFeatures car={props.car} removeFeature={removeFeature} />
       </div>
       <div className="box">
-        <AdditionalFeatures store={props.store} addFeature={ buyItem } />
+        <AdditionalFeatures store={props.store} addFeature={buyItem} />
         <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
-  );
+  )
 };
 
 const mapStateToProps = state => {
   return {
-    additionalPrice: state.additionalPrice,
     car: state.car,
-    store: state.store
+    store: state.store,
+    additionalPrice: state.additionalPrice
   }
 }
 
 export default connect(mapStateToProps, { addFeature, removeFeature })(App);
-
-/* 
-mapStateToProps and { connect } used to connect  */
--- src/actions/index.js
+++ src/actions/index.js
@@ -3,14 +3,22 @@ export const ADD_FEATURE = 'ADD_FEATURE';
 export const REMOVE_FEATURE = 'REMOVE_FEATURE';
 
 // action creators
-export function addFeature() {
+export const addFeature = feature => {
   return {
-    type: ADD_FEATURE
+    type: ADD_FEATURE,
+    payload: feature
   };
 };
 
-export function removeFeature() {
+export const removeFeature = feature => {
   return {
-    TYPE: REMOVE_FEATURE
+    type: REMOVE_FEATURE,
+    payload: feature
   };
 };
+
+/* 
+Create action types; i.e. create variable with the name of the string, assign it the string we would have passed to an action and give action.type the variable as its value (lines 2 & 3)
+
+An action creator is a function that creates an action (lines 6-11 & 13-18)
+*/
